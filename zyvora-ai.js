88f6b2e3216242bfec1a94d39c5eab449ba634d4
Mustapha Gbanie <zyvora490@gmail.com>
2026-09-22 14:57:39 +0000

    Update zyvora-ai.js

--- zyvora-ai.js
+++ zyvora-ai.js
@@ -1,4 +1,14 @@
-/* ==============ZYVORA AI OPENROUTER FRONTEND TEST============== */     /* =======================     1. ALL  SETTINGS NEED       ======================== */const OPENROUTER_API_KEY = "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"; const MODEL_NAME = "openrouter/free"; const API_URL = "https://openrouter.ai/api/v1/chat/completions";
+/* ==============
+ZYVORA AI OPENROUTER FRONTEND TEST
+============== */
+
+/* =======================
+1. ALL  SETTINGS NEED
+======================== */
+const OPENROUTER_API_KEY = "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";
+const MODEL_NAME = "openrouter/free";
+const API_URL = "https://openrouter.ai/api/v1/chat/completions";
+
 /* =========================================
    2. GET HTML ELEMENTS
    ========================================= */
